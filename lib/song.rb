24016ac28b7cf958f042bee977fd5b223b601566
Keith Filkins <kafilkins@me.com>
2020-12-15 01:33:02 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -17,10 +17,13 @@ class Song
   end
 
   def self.new_by_name(name)
-    song = self.create
+    song = self.new
     song.name = name
     song
-
+  end
+  
+  def self.create_by_name
+    
   end
 
 
